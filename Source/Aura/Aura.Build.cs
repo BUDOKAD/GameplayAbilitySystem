5d35d216ab52d0701aab8b1fec8694f7f5b6b289
jszewczyk <jurek@imagepower.pl>
2024-01-15 23:08:13 +0100

    3.21 done

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -4,14 +4,14 @@ using UnrealBuildTool;
 
 public class Aura : ModuleRules
 {
+	
 	public Aura(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayAbilities" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayTags", "GameplayTasks" });
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
--- Source/Aura/Public/Characters/AuraCharacterBase.h
+++ Source/Aura/Public/Characters/AuraCharacterBase.h
@@ -1,23 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "AbilitySystemInterface.h"
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "AuraCharacterBase.generated.h"
 
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+
 UCLASS(Abstract)
-class AURA_API AAuraCharacterBase : public ACharacter
+class AURA_API AAuraCharacterBase : public ACharacter,public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
-		AAuraCharacterBase();
-
+	AAuraCharacterBase();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	UAttributeSet* getattributeSet() const {return AttributeSet;}
 protected:
 	virtual void BeginPlay() override;
 	
 	UPROPERTY(EditAnywhere, Category="Combat")
 	TObjectPtr<USkeletalMeshComponent> Weapon;
-	
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
 }; 
