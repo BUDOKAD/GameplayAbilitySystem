3c43a1d8d6139acc813ebdce4900352b3bd72f79
jszewczyk <jurek@imagepower.pl>
2024-01-25 18:26:04 +0100

    74 done

--- Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -1,22 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AbilitySystem/ModMagCalc/MMC_MaxHealth.h"
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
 
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "Interaction/CombatInterface.h"
 
-UMMC_MaxHealth::UMMC_MaxHealth()
+UMMC_MaxMana::UMMC_MaxMana()
 {
 	
-	VitalityDef.AttributeToCapture = UAuraAttributeSet::GetVitalityAttribute();
-	VitalityDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
-	VitalityDef.bSnapshot = false;
+	InteligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	InteligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	InteligenceDef.bSnapshot = false;
 
-	RelevantAttributesToCapture.Add(VitalityDef);
+	RelevantAttributesToCapture.Add(InteligenceDef);
 }
 
-float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
 {
 	//gather tags from source and target
 	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
@@ -25,14 +25,14 @@ float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffec
 	EvaluationParameters.SourceTags= SourceTags;
 	EvaluationParameters.TargetTags= TargetTags;
 	
-	float Vitality = 0.f;
-	GetCapturedAttributeMagnitude(VitalityDef,Spec,EvaluationParameters,Vitality);
-	Vitality = FMath::Max<float>(Vitality, 0.f);
+	float Inteligence = 0.f;
+	GetCapturedAttributeMagnitude(InteligenceDef,Spec,EvaluationParameters,Inteligence);
+	Inteligence = FMath::Max<float>(Inteligence, 0.f);
 
 	ICombatInterface* CombatInterface=Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
 	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
 
 
 	//MatH 
-	return 80.8 + 2.5f * Vitality + 10.f * PlayerLevel;
-}	
+ 	return 80.8 + 2.5f * Inteligence + 15.f * PlayerLevel;
+}
